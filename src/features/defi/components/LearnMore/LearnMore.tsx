116c87de9a955ac525f836b4f87293f7df465434
Alexandre Gomes <17035424+gomesalexandre@users.noreply.github.com>
2022-02-09 14:01:56 +0300

    wip

--- src/features/defi/components/LearnMore/LearnMore.tsx
+++ src/features/defi/components/LearnMore/LearnMore.tsx
@@ -1,25 +1,60 @@
 import { Box, Flex } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/react'
-import { useHistory, useLocation, useParams } from 'react-router-dom'
+import { useSteps } from 'chakra-ui-steps'
+import { useHistory, useLocation } from 'react-router-dom'
 import osmosis from 'assets/osmosis.svg'
 import { Text } from 'components/Text'
 
 import { DefiModalHeader } from '../DefiModalHeader/DefiModalHeader'
 
+const STEP_TO_ELEMENTS_MAPPING = {
+  1: {
+    bodies: [
+      'defi.learnMore.bodies.rateFluctuationInfo',
+      'defi.learnMore.bodies.amountStakingInfo',
+      'defi.learnMore.bodies.withdrawInfo'
+    ],
+    headerImageSrc: osmosis,
+    headerImageWidth: '120px'
+  },
+  2: {
+    bodies: ['defi.learnMore.bodies.unbondingInfo'],
+    headerImageSrc: osmosis,
+    headerImageWidth: '120px'
+  },
+  3: {
+    bodies: ['defi.learnMore.bodies.slashingInfo', 'defi.learnMore.bodies.partnerInfo'],
+    headerImageSrc: osmosis,
+    headerImageWidth: '120px'
+  }
+}
 export const LearnMore = () => {
   const history = useHistory()
   const location = useLocation()
-  const params = useParams()
 
-  const handleNextClick = () => {
+  const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+    initialStep: 1
+  })
+
+  console.log({ location })
+
+  const isLastStep = activeStep === 3
+
+  const handleNextOrCloseClick = () => {
+    if (isLastStep) {
+      return history.push({
+        pathname: `/assets/ethereum`,
+        state: { background: location }
+      })
+    }
+
+    nextStep()
     history.push({
       pathname: `/defi/vault/cosmos/learn-more`,
-      search: '?step=2',
+      search: `?step=${activeStep + 1}`,
       state: { background: location }
     })
   }
-
-  console.log({ location })
 
   return (
     <Box pt='36px' pb='20px' px='24px'>
@@ -32,37 +67,23 @@ export const LearnMore = () => {
       >
         <DefiModalHeader
           headerImageSrc={osmosis}
-          headerText={'defi.learnMore.headers.0'}
+          headerText={`defi.learnMore.headers.${activeStep - 1}`}
           headerImageWidth='120px'
         />
-        <Box textAlign='left'>
-          <Text
-            translation='defi.learnMore.bodies.rateFluctuationInfo'
-            color='gray.500'
-            fontWeight='semibold'
-            fontSize='15px'
-          />
-        </Box>
-        <Box textAlign='left'>
-          <Text
-            translation='defi.learnMore.bodies.amountStakingInfo'
-            color='gray.500'
-            fontWeight='semibold'
-            fontSize='15px'
-          />
-        </Box>
-        <Box textAlign='left'>
-          <Text
-            translation='defi.learnMore.bodies.withdrawInfo'
-            color='gray.500'
-            fontWeight='semibold'
-            fontSize='15px'
-          />
-        </Box>
-
+        {STEP_TO_ELEMENTS_MAPPING[activeStep].bodies.map(body => (
+          <Box textAlign='left'>
+            <Text translation={body} color='gray.500' fontWeight='semibold' fontSize='15px' />
+          </Box>
+        ))}
         <Box width='100%'>
-          <Button size='lg' zIndex={1} width='100%' colorScheme='blue' onClick={handleNextClick}>
-            <Text translation='defi.learnMore.next' />
+          <Button
+            size='lg'
+            zIndex={1}
+            width='100%'
+            colorScheme='blue'
+            onClick={handleNextOrCloseClick}
+          >
+            <Text translation={isLastStep ? 'defi.learnMore.close' : 'defi.learnMore.next'} />
           </Button>
         </Box>
       </Flex>
